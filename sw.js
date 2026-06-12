45bf00723f10ee921407c70a39d665d1e07c5ecd
Greatben64 <benoit.cliche@yahoo.ca>
2026-06-12 19:26:19 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'kite-iles-v1';
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.includes('api.open-meteo.com')) {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request))
+    );
+  }
+});
