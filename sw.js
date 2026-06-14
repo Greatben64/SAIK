c88fb5d85c136eb8a6fb31d4cc79c40c9650ef56
Greatben64 <benoit.cliche@yahoo.ca>
2026-06-13 23:39:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spots-iles-v2';
+const CACHE = 'spots-iles-v3';
 const SHELL = ['./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
